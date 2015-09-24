812fd95f101916c9c0183613064c9ccf8e6c1ae7
ogidoshuta <syuta.ogido@gmail.com>
2015-09-24 21:08:07 +0900

    Revert "デザイン変更　バグ修正"
    This reverts commit 81266a0064e18e0de3b5063ae73a317160244022.

--- proxy-changer/background.js
+++ proxy-changer/background.js
@@ -1,64 +1,33 @@
-var targetObj;
+window.onload=function(){
+  document.getElementById("scholl").addEventListener("click", changeShollProxy);
+  document.getElementById("outer").addEventListener("click", changeOuterProxy);
+}
+
 var config = {
     mode: "fixed_servers",
     rules: {
       proxyForHttp: {
         scheme: "http",
-        host: "xx.xx.xx.xx",
+        host: "x.x.x.xx",
         port: xxxx
       },
       bypassList: ["127.0.0.1"]
     }
   };
 
-window.onload=function(){
-  document.getElementById("scholl").addEventListener("click", changeShollProxy);
-  document.getElementById("auto-detect").addEventListener("click", changeAutoDetectProxy);
 
-  chrome.storage.sync.get(null, function(items){console.log(items)});
-  chrome.proxy.settings.get({'incognito': false}, function(obj){
-    console.log(obj);
-    if (obj.value.mode == "auto_detect"){
-      targetObj = document.getElementsByName("auto-detect")[0];
-      targetObj.id = "target";
-    } else {
-      chrome.storage.sync.get(null, function(items){
-        
-        for(key in items){
-          console.log(items[key]);
-          if (items[key] == obj.value.rules.proxyForHttp.host){
-            targetObj = document.getElementsByName(key)[0];        
-          }
-        }
-        targetObj.id = "target";
-      });
-    }
-    
-  });
-}
-
-function changeShollProxy(obj){
-  config.mode = "fixed_servers";
-  changeTarget(obj.target.parentNode);
+function changeShollProxy(){
   chrome.proxy.settings.set(
     {value: config, scope: "regular"},
-    function(){}
+    function(){window.close();}
   );
 }
 
-function changeAutoDetectProxy(obj){
+function changeOuterProxy(){
   config.mode = "auto_detect";
-  changeTarget(obj.target.parentNode);
-  
   chrome.proxy.settings.set(
     {value: config, scope: "regular"},
-    function(){
-    }
+    function(){window.close()}
   );
-}
 
-function changeTarget(newTarget){
-  targetObj.id = "";
-  targetObj = newTarget;
-  targetObj.id = "target";
 }
