065cdf26f2028b15b36337c692293e93d7e07b14
ogidoshuta <syuta.ogido@gmail.com>
2015-09-24 21:17:33 +0900

    修正

--- proxy-changer/background.js
+++ proxy-changer/background.js
@@ -1,33 +1,64 @@
-window.onload=function(){
-  document.getElementById("scholl").addEventListener("click", changeShollProxy);
-  document.getElementById("outer").addEventListener("click", changeOuterProxy);
-}
-
+var targetObj;
 var config = {
     mode: "fixed_servers",
     rules: {
       proxyForHttp: {
         scheme: "http",
-        host: "x.x.x.xx",
+        host: "xx.xx.xx.xx",
         port: xxxx
       },
       bypassList: ["127.0.0.1"]
     }
   };
 
+window.onload=function(){
+  document.getElementById("scholl").addEventListener("click", changeShollProxy);
+  document.getElementById("auto-detect").addEventListener("click", changeAutoDetectProxy);
 
-function changeShollProxy(){
+  chrome.storage.sync.get(null, function(items){console.log(items)});
+  chrome.proxy.settings.get({'incognito': false}, function(obj){
+    console.log(obj);
+    if (obj.value.mode == "auto_detect"){
+      targetObj = document.getElementsByName("auto-detect")[0];
+      targetObj.id = "target";
+    } else {
+      chrome.storage.sync.get(null, function(items){
+        
+        for(key in items){
+          console.log(items[key]);
+          if (items[key] == obj.value.rules.proxyForHttp.host){
+            targetObj = document.getElementsByName(key)[0];        
+          }
+        }
+        targetObj.id = "target";
+      });
+    }
+    
+  });
+}
+
+function changeShollProxy(obj){
+  config.mode = "fixed_servers";
+  changeTarget(obj.target.parentNode);
   chrome.proxy.settings.set(
     {value: config, scope: "regular"},
-    function(){window.close();}
+    function(){}
   );
 }
 
-function changeOuterProxy(){
+function changeAutoDetectProxy(obj){
   config.mode = "auto_detect";
+  changeTarget(obj.target.parentNode);
+  
   chrome.proxy.settings.set(
     {value: config, scope: "regular"},
-    function(){window.close()}
+    function(){
+    }
   );
+}
 
+function changeTarget(newTarget){
+  targetObj.id = "";
+  targetObj = newTarget;
+  targetObj.id = "target";
 }
